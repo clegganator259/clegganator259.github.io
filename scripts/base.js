3c8231be1a557259def200557480e132782f4176
clegganator259 <djclegg01@hotmail.com>
2014-07-06 18:07:42 +0100

    adding files
    Adding files

---
+++ scripts/base.js
@@ -0,0 +1,26 @@
+var addressTabel = {home_link: "Home", samples_link: "Samples", software_link:"Software", events_link:"Events", academic_link: "Academic_Info"};
+var colorTable = {home_link:"#FF0000", samples_link: "#33CC33", software_link:"#0066CC", events_link:"#FFFF00",academic_link:"#7A00CC"};
+var css_template = '<style type="text/css">b,a{color:<<COLOR>>}</style>';
+
+
+function loadContent(index){
+	//gets the address of the content to be loaded
+	var newPage = addressTabel[index];
+	var data_container = document.getElementById("data_container");
+	if(data_container.getAttribute("current-data") !== newPage){
+		document.title = "Chem Access: " + newPage.replace("_", " ");
+		data_container.setAttribute('current-data', newPage);
+		requestData(newPage + ".html",function (xHTML){changeContent(xHTML,colorTable[index]);});
+	}
+}
+
+function changeContent(xHTML,color){
+	var container = document.getElementById('data_container');
+	$(container).fadeTo('600',0,function() {
+		$(this).html(create_css_header(color) + xHTML.responseText).fadeTo('600',1);
+	});
+}
+
+function create_css_header(color){
+	return css_template.replace("<<COLOR>>",color);
+}
---
+++ scripts/request_data.js
@@ -0,0 +1,23 @@
+function requestData(url, callBack)
+{
+	// Create a new XMLHttpRequest object
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		// XMLHttpRequest is supported
+		xmlhttp = new XMLHttpRequest();
+	}
+	else {
+		// Create an ActiveX Object
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	} 
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			callBack(xmlhttp);
+		}
+	}
+	
+	// Open the object with the filename
+	xmlhttp.open("POST", url, false);
+	// Send the request
+	xmlhttp.send(null);
+}
